f29864973533e1e2fe032951ada89754d552930e
Bailey DeSouza <bdesouza@email.unc.edu>
2023-03-13 16:49:16 -0400

    added operational requirement endpoints 8 and 9.

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const args = minimist(process.argv.slice(2));
 const port = args.port || 5000
 
 const app = express()
-app.use(express.json());
-app.use(express.urlencoded());
 
 // use cors HTTP headers
 app.use(cors())
@@ -29,11 +27,17 @@ app.get('/app/rpsls/', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls()))
 });
 
-// plays rps game and accepts shots in form of URLEncoded or JSON
-// returns {"player": "(rock|paper|scissors)", "opponent": "(rock|paper|scissors)", "result": "(win|lose|tie)"}
-app.get('/app/rps/play/', (req, res) => {
-    
+// rps endpoint /app/rps/play/(rock|paper|scissors) 
+// returns {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rps/play/:shot/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)))
 });
+
+// rpsls endpoint /app/rpsls/play/(rock|paper|scissors|spock|lizard)
+// returns {"player":"(rock|paper|scissors|lizard|spock)","opponent":"(rock|paper|scissors|lizard|spock)","result":"(win|lose|tie)"}
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
+})
 
 app.listen(port, () => {
 	console.log("Server listening on port " + port);
