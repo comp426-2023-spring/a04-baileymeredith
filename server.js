9f44815a6f61e1970702895e2d5549901c12b295
Bailey DeSouza <bdesouza@email.unc.edu>
2023-03-13 12:46:49 -0400

    installed express and started setting up express app.

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-import {rps, rpsls} from "../lib/rpsls.js"
+import {rps, rpsls} from "./lib/rpsls.js"
 import minimist from "minimist"
+import express from "express"
+
+const args = minimist(process.argv.slice(2));
+// if no port is given, default to port 5000
+const port = args.port || 5000
+
+const app = express()
+app.listen(port, () => 
+    console.log("App ran on port " + port)
+);
