84f4f06d2b1ed43e49e828c40a08451aa6e5a1ef
Bailey DeSouza <bdesouza@email.unc.edu>
2023-03-13 15:53:44 -0400

    edited endpoint /app/ and wrote endpoints /app/rps/ and /app/rpsls/

--- server.js
+++ server.js
@@ -9,13 +9,22 @@ const port = args.port || 5000
 
 const app = express()
 
+// use cors HTTP headers
 app.use(cors())
 
 // check endpoint that returns 200 OK
 app.get('/app/', (req, res) => {
-    res.sendStatus(200);
+    res.sendStatus(200)
 });
 
-app.listen(port, () => 
-    console.log("App ran on port " + port),
-);
+// rps endpoint returns {"player": "(rock|paper|scissors)""}
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()))
+});
+
+// rpsls endpoint returns {"player": "(rock|paper|scissors|spock|lizard)"}
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()))
+});
+
+app.listen(port)
